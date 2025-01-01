aeead3b2eff9ac8015e87ddc3f8ec204847a5dd7
Avi <avirat.patel@gmail.com>
2025-01-01 16:17:16 +0530

    initial-commit

---
+++ readme.txt
@@ -0,0 +1,7 @@
+My first Sample Struts 1.3.10 project
+
+Working URL for Landing Page: http://localhost:8080/struts1-sample-avi/
+
+TO DO with mappings in the struts-config.xml file
+
+http://localhost:8080/struts1-sample-avi/helloWorld.do
---
+++ src/com/devigroups/struts1/form/HelloWorldForm.java
@@ -0,0 +1,15 @@
+package com.devigroups.struts1.form;
+
+import org.apache.struts.action.ActionForm;
+
+public class HelloWorldForm extends ActionForm {
+    String hello;
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
